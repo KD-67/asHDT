81d98b3642bda494a3f9aeed752fc2bf88013f04
KD-67 <kevin.a.dominey@gmail.com>
2026-03-04 15:20:47 +0200

    added some random cool CSS buttons i found

--- frontend/src/components/data_management_portal.svelte
+++ frontend/src/components/data_management_portal.svelte
@@ -9,9 +9,22 @@ import DatasetManagement from "./dataset_management.svelte";
 <div id="test_div">TESTING DATA MANAGEMENT PORTAL</div>
 
 <div id="main_container">
-    <a href="http://localhost:5173/#new_user_form">Subject Management</a>
-    <a href="http://localhost:5173/#add_module_page">Module Management</a>
-    <a href="http://localhost:5173/#dataset_management">Dataset Management</a>
+
+    <div class="subpage_container">
+        <div>Click here to edit the subject list:</div>
+        <a class="subpage_button" href="http://localhost:5173/#new_user_form">Subject Management</a>
+    </div>
+    
+    <div class="subpage_container">
+        <div>Click here to edit the module list:</div>
+        <a class="subpage_button" href="http://localhost:5173/#add_module_page">Module Management</a>
+    </div>
+    
+    <div class="subpage_container">
+        <div>Click here to edit datasets:</div>
+        <a class="subpage_button" href="http://localhost:5173/#dataset_management">Dataset Management</a>
+    </div>
+       
 </div>
 
 </main>
@@ -24,6 +37,34 @@ import DatasetManagement from "./dataset_management.svelte";
     display: flex;
     justify-content: space-evenly;
     padding: 1rem;
+}
+
+.subpage_button {
+  background-color: #fbeee0;
+  border: 2px solid #422800;
+  border-radius: 30px;
+  box-shadow: #422800 4px 4px 0 0;
+  color: #422800;
+  cursor: pointer;
+  display: inline-block;
+  font-weight: 600;
+  font-size: 18px;
+  padding: 0 18px;
+  line-height: 50px;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.subpage_button:hover {
+  background-color: #45c8fc;
+}
+
+.subpage_button:active {
+  box-shadow: #422800 2px 2px 0 0;
+  transform: translate(2px, 2px);
 }
 
 </style>
